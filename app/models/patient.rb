3a62e29e1b1f32be6f113b66a7903110bd902b63
Andy Gregorowicz <andrewg@mitre.org>
2008-09-02 20:16:36 +0000

    adding validation to the PID segement and the overall message

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,6 +2,8 @@ class Patient < ActiveRecord::Base
   has_many :observations
   has_one :message_header
   
+  include ValidationHelper
+  
   def to_message
     msg = HL7::Message.new
     
@@ -9,9 +11,10 @@ class Patient < ActiveRecord::Base
     
     pid = HL7::Message::Segment::PID.new 
     pid.patient_id_list = self.identifier
-    pid.patient_name = "#{self.family_name}^#{self.given_name}"
+    pid.patient_name = self.patient_name_field
+    pid.patient_dob = self.date_of_birth.to_formatted_s(:hl7_ts)
     pid.admin_sex = self.administrative_sex
-    pid.address = "#{self.street_address}^^#{self.city}^#{self.state}^#{self.postal_code}"
+    pid.address = self.address_field
     pid.phone_home = self.home_phone
     pid.phone_business = self.business_phone
     
@@ -22,5 +25,38 @@ class Patient < ActiveRecord::Base
     end
     
     msg
+  end
+  
+  def patient_name_field
+    "#{self.family_name}^#{self.given_name}"
+  end
+  
+  def address_field
+    "#{self.street_address}^^#{self.city}^#{self.state}^#{self.postal_code}"
+  end
+  
+  def validate_message(msg)
+    error_list = []
+    
+    error_list.concat(message_header.validate_msh_segment(msg[:MSH]))
+    error_list.concat(validate_pid_segment(msg[:PID]))
+
+    # Need to match the obx to the observation to validate
+
+    error_list
+  end
+  
+  def validate_pid_segment(pid_segment)
+    error_list = []
+    
+    validate_field(self.identifier, pid_segment.patient_id_list, 'PID-3', error_list)
+    validate_field(self.patient_name_field, pid_segment.patient_name, 'PID-5', error_list)
+    validate_field(self.date_of_birth.to_formatted_s(:hl7_ts), pid_segment.patient_dob, 'PID-7', error_list)
+    validate_field(self.administrative_sex, pid_segment.admin_sex, 'PID-8', error_list)
+    validate_field(self.address_field, pid_segment.address, 'PID-11', error_list)
+    validate_field(self.home_phone, pid_segment.phone_home, 'PID-13', error_list)
+    validate_field(self.business_phone, pid_segment.phone_business, 'PID-14', error_list)
+    
+    error_list
   end
 end
--- test/unit/patient_test.rb
+++ test/unit/patient_test.rb
@@ -9,7 +9,38 @@ class PatientTest < ActiveSupport::TestCase
     should 'generate a hl7 v2.5.1 message' do
       message = @patient.to_message
       assert(message)
+      assert_equal('00000111^^^^AN', message[:PID].patient_id_list)
       assert_equal('Thompson^Jennifer', message[:PID].patient_name)
+      assert_equal('19770410', message[:PID].patient_dob)
+      assert_equal('F', message[:PID].admin_sex)
+      assert_equal("2300 Commonwealth Avenue^^Brighton^MA^02135", message[:PID].address)
+    end
+    
+    should 'validate a hl7 v2.5.1 message' do
+      message = @patient.to_message
+      assert(message)
+      error_list = @patient.validate_message(message)
+      assert(error_list)
+      assert(error_list.empty?)
+    end
+    
+    should 'validate a hl7 v2.5.1 PID segment' do
+      message = @patient.to_message
+      assert(message)
+      error_list = @patient.validate_pid_segment(message[:PID])
+      assert(error_list)
+      assert(error_list.empty?)
+    end
+    
+    should 'fail a hl7 v2.5.1 PID segment with bad data' do
+      message = @patient.to_message
+      assert(message)
+      message[:PID].admin_sex = 'M'
+      error_list = @patient.validate_pid_segment(message[:PID])
+      assert(error_list)
+      error = error_list.first
+      assert(error)
+      assert_equal('PID-8', error.location)
     end
   end
 end
