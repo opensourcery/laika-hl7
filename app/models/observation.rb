19dde4ee910aabfbcd8218252f1123dab26ac513
Andy Gregorowicz <andrewg@mitre.org>
2008-09-02 18:30:52 +0000

    implemented validation of observations

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,14 +1,31 @@
 class Observation < ActiveRecord::Base
   belongs_to :patient
   
+  include ValidationHelper
+  
   def to_obx
     obx = HL7::Message::Segment::OBX.new 
     obx.value_type = self.value_type
-    obx.observation_id = "#{self.identifier}^#{self.identifier_text}^#{self.identifier_code_system}"
-    obx.units = self.units
+    obx.observation_id = self.observation_id_field
     obx.observation_value = self.observation_value
+    obx.units = self.units
     obx.references_range = self.reference_range
     
     obx
+  end
+  
+  def observation_id_field
+    "#{self.identifier}^#{self.identifier_text}^#{self.identifier_code_system}"
+  end
+  
+  def validate_obx_segment(obx_segment)
+    error_list = []
+    
+    validate_field(self.value_type, obx_segment.value_type, 'OBX-2', error_list)
+    validate_field(self.observation_id_field, obx_segment.observation_id, 'OBX-3', error_list)
+    validate_field(self.observation_value, obx_segment.observation_value, 'OBX-5', error_list)
+    validate_field(self.units, obx_segment.units, 'OBX-6', error_list)
+    validate_field(self.reference_range, obx_segment.references_range, 'OBX-7', error_list)    
+    error_list
   end
 end
--- test/unit/observation_test.rb
+++ test/unit/observation_test.rb
@@ -1,8 +1,36 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ObservationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  context 'A Observation Instance' do
+    setup do 
+      @patient = Patient.find_by_identifier('00000111^^^^AN')
+      @observation = @patient.observations.first
+    end
+    
+    should 'generate a hl7 v2.5.1 OBX segment' do
+      obx_segment = @observation.to_obx
+      assert(obx_segment)
+      assert_equal('CE', obx_segment.value_type)
+      assert_equal('785-6^MCH^LN', obx_segment.observation_id)
+      assert_equal('32.6', obx_segment.observation_value)
+      assert_equal('pg', obx_segment.units)
+    end
+    
+    should 'validate a hl7 v2.5.1 OBX segment' do
+      obx_segment = @observation.to_obx
+      error_list = @observation.validate_obx_segment(obx_segment)
+      assert(error_list)
+      assert(error_list.empty?)
+    end
+    
+    should 'fail a hl7 v2.5.1 OBX segment with bad data' do
+      obx_segment = @observation.to_obx
+      obx_segment.units = 'gallons'
+      error_list = @observation.validate_obx_segment(obx_segment)
+      assert(error_list)
+      error = error_list.first
+      assert(error)
+      assert_equal('OBX-6', error.location)
+    end
   end
 end
