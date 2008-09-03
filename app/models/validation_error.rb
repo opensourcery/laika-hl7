5ff4771d026f9c5031429fd6e86ef34683a8fc18
Andy Gregorowicz <andrewg@mitre.org>
2008-09-03 19:55:50 +0000

    Showing the validation_errors on the received_messages screen Putting real data in the validation_errors fixture so that the default received_messages screen looks right

--- app/models/validation_error.rb
+++ app/models/validation_error.rb
@@ -1,2 +1,3 @@
 class ValidationError < ActiveRecord::Base
+  belongs_to :received_message
 end
--- app/streamlined/received_message_ui.rb
+++ app/streamlined/received_message_ui.rb
@@ -10,6 +10,15 @@ end
 ReceivedMessage.class_eval { include ReceivedMessageAdditions }
 
 Streamlined.ui_for(ReceivedMessage) do
+  quick_new_button false
+  quick_edit_button false
+  quick_delete_button false
+  
   user_columns :name, :created_at, :passed_validation,
-               :see_raw_message, {:allow_html => true}
+               :see_raw_message, {:allow_html => true},
+               :validation_errors, {:read_only => true,
+                                    :show_view => [:list, 
+                                              {:fields => [:location, :message]}
+                                             ]
+                                   }
 end   
