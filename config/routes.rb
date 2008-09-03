9707240e79154d103cf8ba2c94f468ec1de61b79
Rob McCready <rmccready@mitre.org>
2008-09-03 13:30:19 +0000

    set the default context to the patient list

--- config/routes.rb
+++ config/routes.rb
@@ -29,9 +29,9 @@ ActionController::Routing::Routes.draw do |map|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
   #   end
-
+  
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "patients"
 
   # See how all your routes lay out with "rake routes"
 
