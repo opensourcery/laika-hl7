e2ce12bd5fd6ab6bbf0cbb4f5e1139afd3d08ad0
Rob McCready <rmccready@mitre.org>
2008-09-03 21:03:13 +0000

    Tweaks to the UI

--- app/streamlined/patient_ui.rb
+++ app/streamlined/patient_ui.rb
@@ -38,6 +38,6 @@ Streamlined.ui_for(Patient) do
                 :observations, {:show_view => [:list, {:fields => [:observation_value, :units]} ], 
                                 :edit_view => [:window, {:fields => [:observation_value, :units]} ] },
                 :message_header, {:show_view => [:name, {:fields => [:sending_application, :sending_facility], :separator => " " }, ],
-                                  :edit_view => [:select]},
+                                  :edit_view => [:select, { :fields => [:sending_application, :sending_facility], :separator => " " }, ]},
                 :mirth, {:allow_html => true}
 end
